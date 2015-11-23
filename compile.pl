98eebe9007fd26b1a68bcbe0d5c7ae5f3ec10a42
Ilan Kleiman <ilankleiman@gmail.com>
2015-11-23 18:29:52 -0500

    Master script - executes all others

---
+++ compile.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/perlml
+
+use CGI;
+use File::Slurp;
+use DBI;
+
+BEGIN
+{
+	$cgi = new CGI;
+	print $cgi->header(-type=>'text/html', -status=>'200 OK');
+	open(STDERR, ">&STDOUT");
+}
+
+my $dbh = DBI->connect("DBI:mysql:database=github; host=localhost", "username", "password!", {'RaiseError' => 1});
+my $sth = $dbh->prepare("TRUNCATE reddit");
+$sth->execute() or die "Couldn't execute statement: $DBI::errstr; stopped";
+$dbh->disconnect();
+	
+	@subreddits = ('starcraft', 'worldnews', 'interestingasfuck', 'todayilearned', 'Jokes', 'pics'); 
+	foreach (@subreddits)
+	{
+		write_file('subreddit.txt', $_);
+		`curl -s "http://ilankleiman.com/newsfeed/fetchtop.pl?sub=".$_`;
+		`curl -s "http://ilankleiman.com/newsfeed/parseandinsert.pl?sub=".$_`;
+	}
+
+print "Updated news :D";
